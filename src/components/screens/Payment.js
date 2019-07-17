46e2098ff1a970250a87b1b344c1701adffc9e05
Benoit Jeaurond <bjeau025@uottawa.ca>
2019-07-17 19:54:55 -0400

    Polished paiement

--- src/components/screens/Payment.js
+++ src/components/screens/Payment.js
@@ -1,217 +1,226 @@
 import React from 'react';
-import { StyleSheet, View, ScrollView, Text, KeyboardAvoidingView } from 'react-native';
+import { StyleSheet, View, ScrollView, Text, KeyboardAvoidingView, Image, TouchableOpacity } from 'react-native';
 import { connect } from 'react-redux';
-import { RadioButton, TextInput, HelperText } from 'react-native-paper';
+import { RadioButton, TextInput, HelperText, Button } from 'react-native-paper';
 import { getStatusBarHeight } from 'react-native-iphone-x-helper';
 import { Header } from 'react-navigation';
-import { Dropdown } from 'react-native-material-dropdown';
 import Touchable from 'react-native-platform-touchable';
+import { CustomTabs } from 'react-native-custom-tabs';
+
+const textInputTheme = {
+  colors: { 
+    primary: "#1C88E5", 
+    background: "#ffffff", 
+    underlineColor: '#1C88E5' 
+  }
+}
+
+const radioButtonTheme = {
+  colors: { 
+    accent: "#1C88E5", 
+  }
+}
 
 class Payment extends React.PureComponent {
   constructor(props) {
     super(props);
 
     this.state = {
-
       paymentState: 'first',
-      months: [
-        { value: 'Janvier', },
-        { value: 'Février', },
-        { value: 'Mars', },
-        { value: 'Avril', },
-        { value: 'Mai', },
-        { value: 'Juin', },
-        { value: 'Juillet', },
-        { value: 'Août', },
-        { value: 'Septembre', },
-        { value: 'Octobre', },
-        { value: 'Novembre', },
-        { value: 'Décembre', }
-      ],
-      years: [
-        { value: '2019' },
-        { value: '2020' },
-        { value: '2021' },
-        { value: '2022' },
-        { value: '2023' },
-        { value: '2024' },
-        { value: '2025' },
-        { value: '2026' },
-        { value: '2027' },
-        { value: '2028' },
-        { value: '2029' },
-        { value: '2030' },
-      ]
     };
   }
 
+  openPayPal = () => {
+    CustomTabs.openURL('https://www.paypal.com/ca/signin', {
+      enableUrlBarHiding: true,
+      showPageTitle: true,
+      enableDefaultShare: true,
+      forceCloseOnRedirection: true,
+    });
+  }
 
-  validate = (paymentState) => {
-
+  validate = () => {
     let isfilled = true;
-    let variables = ['ville', 'numRue', 'nomRue', 'codePostal', 'pays'];
-    if (paymentState == 'second') {
+    let variables = [];
+
+    if (this.state.paymentState == 'second') {
       variables.push('nomComplet')
       variables.push('numCarte')
-
-    }
-    else if (paymentState == 'first') {
+    } else if (this.state.paymentState == 'first') {
       variables.push('nomComplet')
       variables.push('numCarte')
       variables.push('cvv')
     }
-    variables.forEach((i) => {
+/*
+		variables.forEach((i) => {
+			if(this.state[i] == null || this.state[i] == '' ){
+				this.setState({[i]: ''})
+				isfilled = false
+			} 
+		});*/
 
-      if (this.state[i] == null || this.state[i] == '') {
-        this.setState({ [i]: '' })
-        isfilled = false
-      }
-    });
     if (isfilled) {
-      this.props.navigation.navigate("Summary")
-
-
+      this.props.navigation.navigate("Summary");
     }
   }
 
   render() {
-    const { paymentState } = this.state;
-
-
-    let paiementContent = (
-      <View>
-        <Text>Faite certain d'emporter votre argent lors de la commande</Text>
-      </View>
-    )
+    const { paymentState, nomComplet, cvv, numCarte, month, year } = this.state;
 
     let CVV = (<View />);
+    let paiementContent = (
+      <View style={styles.payPalContainer}>
+        <Text>Veuillez vous authentifier avec PayPal pour compléter le paiement</Text>
 
+        <View style={styles.payPalButtonContainer}>
 
-    switch (paymentState) {
-      case 'first':
-        CVV = (
-          <View>
-            <TextInput
-              label='CVV'
-              mode='outlined'
-              style={styles.textInput}
-              value={this.state.cvv}
-              keyboardType="number-pad"
-              onChangeText={cvv => this.setState({ cvv })} />
+          <View style={styles.payPalButton}>
+            <Touchable onPress={this.openPayPal}>
+              <View style={styles.payPalContent}>
 
-            <HelperText
-              type="error"
-              visible={this.state.cvv == ""}
-            >
-              Le cvv est necessaire
-					</HelperText>
+                <Image source={require("../../assets/paypal.png")} 
+                  style={styles.payPalIcon} />
+                <Text>Procéder avec PayPal</Text>
+              </View>
+            </Touchable>
           </View>
-        )
-        break;
-      case 'second':
-        break;
-    }
+        </View>
+      </View>
+    );
 
-    if (paymentState != 'third') {
-
-      paiementContent = (
+    if (paymentState === "first") {
+      CVV = (
         <View>
-
-          <Text style={styles.title}>
-            Information de paiement
-							</Text>
-
-          <Text style={styles.subtitle}>
-            Information générale
-							</Text>
-          <TextInput
-            label='Nom complet'
+          <TextInput theme={textInputTheme}
+            label='CVV'
             mode='outlined'
             style={styles.textInput}
-            value={this.state.nomComplet}
-            onChangeText={nomComplet => this.setState({ nomComplet })} />
-          <HelperText
-            type="error"
-            visible={this.state.nomComplet == ""}
-          >
-            Le Nom au complet est necessaire
-					</HelperText>
-
-          <TextInput
-            label='Numéro de carte'
-            mode='outlined'
-            style={styles.textInput}
-            value={this.state.numCarte}
+            value={cvv}
             keyboardType="number-pad"
-            onChangeText={numCarte => this.setState({ numCarte })} />
+            onChangeText={cvv => this.setState({ cvv })} />
+
           <HelperText
             type="error"
-            visible={this.state.numCarte == ""}
-          >
-            Le numero de la carte est necessaire
-					</HelperText>
-          {CVV}
-
-
-          <Text style={styles.subtitle}>
-            Date d'expiration
-							</Text>
-          <Dropdown
-            label='Mois'
-            selectedItem={(selectedMonth) => this.setState({ selectedMonth })}
-            data={this.state.months} />
-
-          <Dropdown
-            label='Année'
-            selectedItem={(selectedYear) => this.setState({ selectedYear })}
-            data={this.state.years}
-          />
+            visible={cvv == ""} >
+            Le CVV est obligatoire
+          </HelperText>
         </View>
       )
     }
 
+    if (paymentState != 'third') {
+      paiementContent = (
+        <View>
+          <Text style={styles.title}>Information de paiement</Text>
+
+          <Text style={styles.subtitle}>Information générale</Text>
+
+          <TextInput theme={textInputTheme}
+            label='Nom complet'
+            mode='outlined'
+            style={styles.textInput}
+            value={nomComplet}
+            onChangeText={nomComplet => this.setState({ nomComplet })} />
+
+          <HelperText
+            type="error"
+            visible={nomComplet == ""}>
+            Votre nom est obligatoire
+					</HelperText>
+
+          <TextInput theme={textInputTheme}
+            label='Numéro de carte'
+            mode='outlined'
+            style={styles.textInput}
+            value={numCarte}
+            keyboardType="number-pad"
+            onChangeText={numCarte => this.setState({ numCarte })} />
+
+          <HelperText
+            type="error"
+            visible={numCarte == ""}>
+            Le numero de votre carte est obligatoire
+					</HelperText>
+
+          {CVV}
+
+          <Text style={styles.subtitle}>Date d'expiration</Text>
+
+          <View style={styles.dateContainer}>
+            <TextInput theme={textInputTheme}
+              label='Mois'
+              mode='outlined'
+              style={styles.dateInput}
+              value={month}
+              keyboardType="number-pad"
+              onChangeText={month => this.setState({ month })} />
+
+            <TextInput theme={textInputTheme}
+              label='Année'
+              mode='outlined'
+              style={styles.dateInput}
+              value={year}
+              keyboardType="number-pad"
+              onChangeText={year => this.setState({ year })} />
+          </View>
+        </View>
+      )
+    }
 
     return (
       <View style={styles.container}>
         <ScrollView>
-
-          <KeyboardAvoidingView style={styles.content} behavior="padding" enabled>
+          <KeyboardAvoidingView style={styles.content} 
+            behavior="padding" 
+            enabled>
             <View>
               <Text style={[styles.title, { marginTop: 0 }]}>
                 Méthode de paiement
 							</Text>
+
               <View style={styles.radioContainer}>
                 <RadioButton.Group
                   onValueChange={paymentState => this.setState({ paymentState })}
-                  value={this.state.paymentState}>
+                  value={paymentState}>
                   <View style={styles.radio}>
-                    <RadioButton value="first" />
-                    <Text>Crédit</Text>
+                    <RadioButton value="first"
+                      theme={radioButtonTheme}/>
+
+                    <TouchableOpacity onPress={() => this.setState({ paymentState: "first" })}>
+                      <Text style={styles.radioText}>Crédit</Text>
+                    </TouchableOpacity>
                   </View>
+
                   <View style={styles.radio}>
-                    <RadioButton value="second" />
-                    <Text>Débit</Text>
+                    <RadioButton value="second"
+                      theme={radioButtonTheme} />
+
+                    <TouchableOpacity onPress={() => this.setState({ paymentState: "second" })}>
+                      <Text style={styles.radioText}>Débit</Text>
+                    </TouchableOpacity>
                   </View>
+
                   <View style={styles.radio}>
-                    <RadioButton value="third" />
-                    <Text>Paypal</Text>
+                    <RadioButton value="third"
+                      theme={radioButtonTheme} />
+
+                    <TouchableOpacity onPress={() => this.setState({ paymentState: "third" })}>
+                      <Text style={styles.radioText}>PayPal</Text>
+                    </TouchableOpacity>
                   </View>
                 </RadioButton.Group>
               </View>
 
               {paiementContent}
 
-              <View style={styles.buttonContainer}>
-
-                <Touchable style={styles.button}
-                  onPress={() => this.validate(paymentState)}>
-
-                  <Text style={styles.buttonText}>
-                    Voir le sommaire
-  </Text>
-
-                </Touchable>
-              </View>
+              <Button mode='contained'
+                style={styles.buttonContainer}
+                theme={textInputTheme}
+                onPress={this.validate}>
+                <Text style={styles.button}>
+                  Voir le sommaire
+                </Text>
+              </Button>
             </View>
           </KeyboardAvoidingView>
         </ScrollView>
@@ -219,12 +228,12 @@ class Payment extends React.PureComponent {
     );
   }
 }
+
 const mapState = state => {
   return {
     items: state.UserReducer.items
   };
 };
-
 
 export default connect(mapState)(Payment);
 
@@ -237,12 +246,23 @@ const styles = StyleSheet.create({
     padding: 20,
     paddingTop: 10 + getStatusBarHeight() + Header.HEIGHT,
   },
+  dateContainer: { 
+    flexDirection: 'row', 
+    flex: 1, 
+    justifyContent: 'space-between' 
+  },
+  dateInput: { 
+    flex: 0.45 
+  },
   radio: {
     alignItems: 'center',
     justifyContent: 'center',
     flex: 1,
     flexDirection: 'row',
     color: 'black',
+  },
+  radioText: {
+    color: 'black'
   },
   radioContainer: {
     width: '100%',
@@ -264,21 +284,35 @@ const styles = StyleSheet.create({
   button: {
     width: "100%",
     paddingVertical: 10,
-    alignItems: 'center'
-  },
-  buttonText: {
-    textTransform: 'uppercase',
-    fontWeight: "bold",
-    borderRadius: 10,
-    letterSpacing: 1.5,
-    color: 'white'
+    alignItems: 'center',
+    textTransform: 'uppercase'
   },
   buttonContainer: {
-    borderRadius: 10, 
-    width: '100%',
-    backgroundColor: "#1f88e5",
-    marginVertical: 20,
-    overflow: 'hidden',
-    elevation: 5
+    marginVertical: 20
   },
+  subtitle: {
+    marginBottom: 10
+  },
+  payPalContainer: {
+    width: "100%"
+  },
+  payPalButtonContainer: {
+    alignItems: 'center', 
+    marginVertical: 40
+  }, 
+  payPalContent: { 
+    flexDirection: 'row', 
+    padding: 10, 
+    alignItems: 'center' 
+  },
+  payPalButton : { 
+    elevation: 4, 
+    backgroundColor: '#ffffff', 
+    borderRadius: 5 
+  },
+  payPalIcon: { 
+    height: 30, 
+    width: 30, 
+    resizeMode: 'contain' 
+  }
 });
